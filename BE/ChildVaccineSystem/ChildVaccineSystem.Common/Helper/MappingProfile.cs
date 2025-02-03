fc75b75350bb715b0da035c1d2f2a7cabd113592
voleducanh93 <voleducanh93@gmail.com>
2025-02-03 16:53:05 +0700

    CRUD_Vaccine

--- BE/ChildVaccineSystem/ChildVaccineSystem.Common/Helper/MappingProfile.cs
+++ BE/ChildVaccineSystem/ChildVaccineSystem.Common/Helper/MappingProfile.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using ChildVaccineSystem.Data.DTO;
+using ChildVaccineSystem.Data.Entities;
+using ChildVaccineSystem.Repository.Repositories;
+using ChildVaccineSystem.RepositoryContract.Interfaces;
+using ChildVaccineSystem.ServiceContract.Interfaces;
+using ChildVaccineSystem.Services;
+using System.Reflection.PortableExecutable;
 
 namespace ChildVaccineSystem.Common.Helper
 {
