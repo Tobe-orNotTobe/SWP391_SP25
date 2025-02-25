13178902dba15143e4076c0d79f10dd566f7ee85
PawPasta <giabaostrike2004@gmail.com>
2025-02-25 12:09:36 +0700

    Lay cai back-end moi nhat cho nhanh GiaBao tu main

---
+++ BE/ChildVaccineSystem/ChildVaccineSystem.Repository/Repositories/UnitOfWork.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using ChildVaccineSystem.Data.Entities;
+using ChildVaccineSystem.Data.Models;
+using ChildVaccineSystem.RepositoryContract.Interfaces;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace ChildVaccineSystem.Repository.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ChildVaccineSystemDBContext _context;
+
+        public IVaccineRepository Vaccines { get; }
+		public IComboVaccineRepository ComboVaccines { get; }
+		public IComboDetailRepository ComboDetails { get; }
+		public IVaccinationScheduleRepository VaccinationSchedules { get; }
+        public IBookingRepository Bookings { get; private set; }
+        public IBookingDetailRepository BookingDetails { get; private set; }
+		public IInjectionScheduleRepository InjectionSchedules { get; }
+		public IVaccineScheduleDetailRepository VaccineScheduleDetails { get; }
+        public IChildrenRepository Children { get; }
+        public IUserRepository Users { get; }
+        public IVaccineInventoryRepository VaccineInventories { get; }
+        public IPricingPoliciesRepository PricingPolicies { get; }
+        public UnitOfWork(ChildVaccineSystemDBContext context, IVaccineRepository vaccineRepository, IVaccinationScheduleRepository vaccinationScheduleRepository, IComboVaccineRepository comboVaccineRepository, IComboDetailRepository comboDetailRepository, IBookingRepository bookingRepository, IBookingDetailRepository bookingDetailRepository, IInjectionScheduleRepository injectionScheduleRepository, IVaccineScheduleDetailRepository vaccineScheduleDetailRepository, IChildrenRepository childrenRepository, IUserRepository userRepository, IVaccineInventoryRepository vaccineInventories, IPricingPoliciesRepository pricingPolicies)
+        {
+            _context = context;
+            Vaccines = vaccineRepository;
+            ComboVaccines = comboVaccineRepository;
+            ComboDetails = comboDetailRepository;
+			VaccinationSchedules = vaccinationScheduleRepository;
+            Bookings = bookingRepository;
+            BookingDetails = bookingDetailRepository; 
+            InjectionSchedules = injectionScheduleRepository;
+			VaccineScheduleDetails = vaccineScheduleDetailRepository;
+            Children = childrenRepository;
+            Users = userRepository;
+            VaccineInventories = vaccineInventories;
+            PricingPolicies = pricingPolicies;
+
+        }
+
+		public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+		public async Task<IDbContextTransaction> BeginTransactionAsync()
+		{
+			return await _context.Database.BeginTransactionAsync();
+		}
+
+		public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
---
+++ BE/ChildVaccineSystem/ChildVaccineSystem.Service/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using ChildVaccineSystem.Common.Helper;
+using ChildVaccineSystem.Service.Services;
+using ChildVaccineSystem.ServiceContract.Interfaces;
+using ChildVaccineSystem.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace ChildVaccineSystem.Service
+{
+    public static class DependencyInjcection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+
+            services.AddTransient<IVaccineService, VaccineService>();
+            services.AddTransient<IEmailService, EmailService>();
+            services.AddScoped<APIResponse>();
+            services.AddTransient<IComboVaccineService, ComboVaccineService>();
+            services.AddTransient<IAuthService, AuthService>();
+			services.AddTransient<IVaccinationScheduleService, VaccinationScheduleService>();
+            services.AddTransient<IBookingService, BookingService>();
+            services.AddTransient<IChildrenService, ChildrenService>();
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IVaccineInventoryService, VaccineInventoryService>();
+
+            return services;
+        }
+    }
+}
