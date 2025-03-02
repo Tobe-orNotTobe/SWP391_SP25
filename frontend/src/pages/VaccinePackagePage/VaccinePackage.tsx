43688d5803ea450063b29622e40b41e49c162621
PawPasta <giabaostrike2004@gmail.com>
2025-03-02 20:06:06 +0700

    add VaccinePackage.tsx

--- frontend/src/pages/VaccinePackagePage/VaccinePackage.tsx
+++ frontend/src/pages/VaccinePackagePage/VaccinePackage.tsx
@@ -3,10 +3,7 @@ import React from "react";
 import CustomerNavbar from "../../components/Navbar/CustomerNavbar/CustomerNavbar.tsx";
 import Footer from "../../components/Footer/Footer.tsx";
 
-
 const VaccinePackage : React.FC = () => {
-
-
 
 
     return (
