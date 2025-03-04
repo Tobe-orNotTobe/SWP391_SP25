e1bab2fdb6179fe163cb6b93f8fbfefa2912269d
PawPasta <giabaostrike2004@gmail.com>
2025-03-05 05:27:49 +0700

    delete BE to merge BE from main

--- BE/ChildVaccineSystem/ChildVaccineSystem.ServiceContract/Interfaces/IVnPaymentService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ChildVaccineSystem.ServiceContract.Interfaces
-{
-	public interface IVnPaymentService
-	{
-		Task<string> CreatePaymentUrl(int bookingId, string clientIpAddress);
-		Task<bool> PaymentExecute(IDictionary<string, string> vnpayParams);
-	}
-}
