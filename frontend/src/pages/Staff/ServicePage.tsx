b207a0f7d1acd15b4d810924e15b297b8ddb2f62
Tan Phat Tran <chopml1102@gmail.com>
2025-03-05 08:44:42 +0700

    Fix layout

--- frontend/src/pages/Staff/ServicePage.tsx
+++ frontend/src/pages/Staff/ServicePage.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import StaffLayout from "../../components/Layout/StaffLayout/StaffLayout.tsx";
 import VaccinationRecordForm from "../../components/VaccinationRecordForm/VaccinationRecordForm";
+import Staff1Layout from "../../components/Layout/StaffLayout/Stafff1Layout/Staff1Layout.tsx";
 
 const ServicePage: React.FC = () => {
   return (
-    <StaffLayout>
+    <Staff1Layout>
       <VaccinationRecordForm />
-    </StaffLayout>
+    </Staff1Layout>
   );
 };
 
