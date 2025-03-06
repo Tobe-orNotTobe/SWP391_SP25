593883282e0e1f251070bedab8c0a4b143c79584
PawPasta <giabaostrike2004@gmail.com>
2025-03-06 21:35:22 +0700

    merge main

--- BE/ChildVaccineSystem/ChildVaccineSystem.Data/Migrations/20250306141047_Migrations.Designer.cs
+++ BE/ChildVaccineSystem/ChildVaccineSystem.Data/Migrations/20250306141047_Migrations.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChildVaccineSystem.Data.Migrations
 {
     [DbContext(typeof(ChildVaccineSystemDBContext))]
-    [Migration("20250306084444_Migrations")]
+    [Migration("20250306141047_Migrations")]
     partial class Migrations
     {
         /// <inheritdoc />
