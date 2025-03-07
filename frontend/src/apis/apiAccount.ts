39f737cfc6c557b38aae308a6a7a8b136e35e5b0
PawPasta <giabaostrike2004@gmail.com>
2025-03-07 19:53:56 +0700

    merge KhiemGia

--- frontend/src/apis/apiAccount.ts
+++ frontend/src/apis/apiAccount.ts
@@ -1,5 +1,5 @@
 import AxiosInstance from "../utils/axiosInstance.ts";
-import {AccountRequest, UpdateAccountRequest} from "../interfaces/Account.ts";
+import {AccountDetailResponse, AccountRequest, UpdateAccountRequest} from "../interfaces/Account.ts";
 import {
     ConfirmEmailRequest,
     ForgotPasswordRequest,
@@ -8,24 +8,33 @@ import {
     ResetPasswordRequest, ResetPasswordUserProfile, UserProfileUpdate
 } from "../interfaces/Account.ts";
 import axiosInstance from "../utils/axiosInstance";
+import {ApiResponse} from "../interfaces/Layout.ts";
 
-export const apiGetAllUser = async () => {
+export const apiGetAllUser = async (): Promise<ApiResponse<AccountDetailResponse[]>> => {
     try {
-        const response = await AxiosInstance.get("/api/Admin/getAllUsers");
+        const response = await AxiosInstance.get<ApiResponse<AccountDetailResponse[]>>("/api/Admin/getAllUsers");
         return response.data;
-    }catch(err) {
-        console.log("An unexpected error occurred "+err);
-        throw err;
+    }catch(err: any) {
+        return {
+            statusCode: err.response?.data?.statusCode,
+            isSuccess: false,
+            errorMessages: err.response?.data?.errorMessages || ["Có lỗi xảy ra!"],
+            result: null
+        };
     }
 }
 
-export const apiGetUserById = async (id: string) => {
+export const apiGetUserById = async (id: string): Promise<ApiResponse<AccountDetailResponse>> => {
     try {
-        const response = await AxiosInstance.get(`/api/Admin/admin/GetUserById/${id}`);
+        const response = await AxiosInstance.get<ApiResponse<AccountDetailResponse>>(`/api/Admin/admin/GetUserById/${id}`);
         return response.data;
-    }catch(err) {
-        console.log("An unexpected error occurred "+err);
-        throw err;
+    }catch(err: any) {
+        return {
+            statusCode: err.response?.data?.statusCode,
+            isSuccess: false,
+            errorMessages: err.response?.data?.errorMessages || ["Có lỗi xảy ra!"],
+            result: null
+        };
     }
 }
 
@@ -33,9 +42,13 @@ export const apiCreateAccount = async (data: AccountRequest) => {
     try {
         const response = await AxiosInstance.post("/api/Admin/create-account", data);
         return response.data;
-    }catch(err) {
-        console.log("An unexpected error occurred "+err);
-        throw err;
+    }catch(err: any) {
+        return {
+            statusCode: err.response?.data?.statusCode,
+            isSuccess: false,
+            errorMessages: err.response?.data?.errorMessages || ["Có lỗi xảy ra!"],
+            result: null
+        };
     }
 }
 
@@ -43,9 +56,13 @@ export const apiUpdateAccount = async (data: UpdateAccountRequest) => {
     try {
         const response = await AxiosInstance.put("/api/Admin/UpdateUser", data);
         return response.data;
-    }catch(err) {
-        console.log("An unexpected error occurred "+err);
-        throw err;
+    }catch(err: any) {
+        return {
+            statusCode: err.response?.data?.statusCode,
+            isSuccess: false,
+            errorMessages: err.response?.data?.errorMessages || ["Có lỗi xảy ra!"],
+            result: null
+        };
     }
 }
 
@@ -53,29 +70,41 @@ export const apiDeleteAccount = async (id: string) => {
     try {
         const response = await AxiosInstance.delete(`/api/Admin/DeleteUser/${id}`);
         return response.data;
-    }catch(err) {
-        console.log("An unexpected error occurred "+err);
-        throw err;
+    }catch(err: any) {
+        return {
+            statusCode: err.response?.data?.statusCode,
+            isSuccess: false,
+            errorMessages: err.response?.data?.errorMessages || ["Có lỗi xảy ra!"],
+            result: null
+        };
     }
 }
 
 export const apiActiveAccount = async (id: string) => {
     try {
-        const response = await AxiosInstance.get(`/api/Admin/activate/${id}`);
+        const response = await AxiosInstance.put(`/api/Admin/activate/${id}`);
         return response.data;
-    }catch(err) {
-        console.log("An unexpected error occurred "+err);
-        throw err;
+    }catch(err: any) {
+        return {
+            statusCode: err.response?.data?.statusCode,
+            isSuccess: false,
+            errorMessages: err.response?.data?.errorMessages || ["Có lỗi xảy ra!"],
+            result: null
+        };
     }
 }
 
 export const apiDeactivateAccount = async (id: string) => {
     try {
-        const response = await AxiosInstance.delete(`/api/Admin/deactivate/${id}`);
+        const response = await AxiosInstance.put(`/api/Admin/deactivate/${id}`);
         return response.data;
-    }catch(err) {
-        console.log("An unexpected error occurred "+err);
-        throw err;
+    }catch(err: any) {
+        return {
+            statusCode: err.response?.data?.statusCode,
+            isSuccess: false,
+            errorMessages: err.response?.data?.errorMessages || ["Có lỗi xảy ra!"],
+            result: null
+        };
     }
 }
 
