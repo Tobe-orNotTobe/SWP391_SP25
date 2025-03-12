32d52fedcd55f3ade669622ade4efe8dd854b560
giakhiemdt <khiem1371@gmail.com>
2025-03-13 05:10:22 +0700

    Update firebase.

--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -1,15 +1,22 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { initializeApp } from "firebase/app";
+import {
+    VITE_FireBase_API_KEY, VITE_FireBase_App_Id,
+    VITE_FireBase_Auth_Domain, VITE_FireBase_Measurement_Id, VITE_FireBase_Messaging_Sender_Id,
+    VITE_FireBase_Project_Id,
+    VITE_FireBase_Storage_Bucket
+} from "../config/firebaseConfig.ts";
+
 
 const firebaseConfig = {
 
-    apiKey: "AIzaSyBiy4bGHIlQJNlfHyPOkyLAcbku-stoZ50",
-    authDomain: "abc-e21be.firebaseapp.com",
-    projectId: "abc-e21be",
-    storageBucket: "abc-e21be.firebasestorage.app",
-    messagingSenderId: "273803195481",
-    appId: "1:273803195481:web:38784f19e7406778b5ed48",
-    measurementId: "G-FS9ME9Y4KS"
+    apiKey: VITE_FireBase_API_KEY,
+    authDomain: VITE_FireBase_Auth_Domain,
+    projectId: VITE_FireBase_Project_Id,
+    storageBucket: VITE_FireBase_Storage_Bucket,
+    messagingSenderId: VITE_FireBase_Messaging_Sender_Id,
+    appId: VITE_FireBase_App_Id,
+    measurementId: VITE_FireBase_Measurement_Id
 };
 
 // Khởi tạo Firebase
