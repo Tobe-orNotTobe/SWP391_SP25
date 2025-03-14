a56a15c4e85b702cf664c55a7ac6fb2fd8567cdd
PawPasta <giabaostrike2004@gmail.com>
2025-03-14 18:04:46 +0700

    migrations again

--- BE/ChildVaccineSystem/ChildVaccineSystem.Data/Migrations/20250314110241_Migrations.Designer.cs
+++ BE/ChildVaccineSystem/ChildVaccineSystem.Data/Migrations/20250314110241_Migrations.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChildVaccineSystem.Data.Migrations
 {
     [DbContext(typeof(ChildVaccineSystemDBContext))]
-    [Migration("20250313162214_Migrations")]
+    [Migration("20250314110241_Migrations")]
     partial class Migrations
     {
         /// <inheritdoc />
