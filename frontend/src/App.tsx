28208de6d228675e9b92da42124613990a86136d
Tan Phat Tran <chopml1102@gmail.com>
2025-03-27 18:26:29 +0700

    fix route and update stafblog manager

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -77,8 +77,6 @@ import AdminRefund from "./pages/Admin/AdminRefund/AdminRefund.tsx";
 import StaffBlogPostPage from "./pages/Staff/StaffBlogPost.tsx";
 import StaffBlogManagePage from "./pages/Staff/Blog/StaffBlogManage/StaffBlogManage.tsx";
 import DoctorProfile from "./pages/Doctor/DoctorProfile/DoctorProfile.tsx";
-import StaffBlogForm from "./pages/Staff/Blog/StaffBogForm.tsx";
-import DoctorBlogForm from "./pages/Doctor/Blog/DoctorBogForm.tsx";
 
 
 function App() {
@@ -136,14 +134,14 @@ function App() {
                 <Route path="/staff/booking" element={<ProtectedRoute allowedRoles={["Staff"]}><BookingForStaff /></ProtectedRoute>} />
                 <Route path="/staff/blogPost" element={<ProtectedRoute allowedRoles={["Staff"]}><StaffBlogPostPage /></ProtectedRoute>} />
                 <Route path="/staff/blogManager" element={<ProtectedRoute allowedRoles={["Staff"]}><StaffBlogManagePage /></ProtectedRoute>} />
-                <Route path="/staff/blog/edit/:id" element={<ProtectedRoute allowedRoles={["Staff"]}><StaffBlogForm /></ProtectedRoute>} />
+                <Route path="/staff/blog/edit/:id" element={<ProtectedRoute allowedRoles={["Staff"]}><StaffBlogPostPage /></ProtectedRoute>} />
                 {/* Doctor Routes */}
                 <Route path="/doctor/vaccination-schedule" element={<ProtectedRoute allowedRoles={["Doctor"]}><VaccinationSchedulePage /></ProtectedRoute>} />
                 <Route path="/doctor/service" element={<ProtectedRoute allowedRoles={["Doctor"]}><ServicePage /></ProtectedRoute>} />
                 <Route path="/doctor/blogPost" element={<ProtectedRoute allowedRoles={["Doctor"]}><DoctorBlogPostPage /></ProtectedRoute>} />
                 <Route path="/doctor/blogManager" element={<ProtectedRoute allowedRoles={["Doctor"]}><DoctorBlogManagePage /></ProtectedRoute>} />
                 <Route path="/doctor/profile" element={<ProtectedRoute allowedRoles={["Doctor"]}><DoctorProfile /></ProtectedRoute>}/>
-                <Route path="/doctor/blog/edit/:id" element={<ProtectedRoute allowedRoles={["Doctor"]}><DoctorBlogForm /></ProtectedRoute>} />
+                <Route path="/doctor/blog/edit/:id" element={<ProtectedRoute allowedRoles={["Doctor"]}><DoctorBlogPostPage /></ProtectedRoute>} />
                 {/* Manager Routes */}
                 <Route path="/manager/dashboard" element={<ProtectedRoute allowedRoles={["Manager"]}><ManagerDashBoard /></ProtectedRoute>} />
                 <Route path="/manager/vaccines" element={<ProtectedRoute allowedRoles={["Manager"]}><ManagerVaccinePage /></ProtectedRoute>} />
